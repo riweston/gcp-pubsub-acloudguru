7e56475b160b844261b8dead8071669c072cfdd5
Richard Weston <github@riweston.io>
2022-12-12 14:54:56 +0000

    wip: Disable the schema for dev purposes

--- infra/pubsub.tf
+++ infra/pubsub.tf
@@ -1,4 +1,4 @@
-resource "google_pubsub_schema" "this" {
+/*resource "google_pubsub_schema" "this" {
   name       = "acg-request"
   type       = "AVRO"
   definition = jsonencode(
@@ -17,17 +17,18 @@ resource "google_pubsub_schema" "this" {
       ]
     }
   )
-}
+}*/
 
 resource "google_pubsub_topic" "this" {
   name = "acg-request"
-  schema_settings {
-    schema   = google_pubsub_schema.this.id
-    encoding = "JSON"
-  }
+  /*  schema_settings {
+      schema   = google_pubsub_schema.this.id
+      encoding = "JSON"
+    }*/
 }
 
 resource "google_pubsub_subscription" "this" {
-  name  = "acg-request"
-  topic = google_pubsub_topic.this.name
+  name                 = "acg-request"
+  topic                = google_pubsub_topic.this.name
+  ack_deadline_seconds = 600
 }
