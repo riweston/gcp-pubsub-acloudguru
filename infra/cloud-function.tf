f0deee683fe186eb202991fd11b8d877435624af
Richard Weston <github@riweston.io>
2022-12-16 10:51:04 +0000

    refactor: Infra cleaned up

--- infra/cloud-function.tf
+++ infra/cloud-function.tf
@@ -1,64 +1,100 @@
 locals {
-  function_name_http_trigger = "http-trigger"
-  location                   = "europe-west2"
+  function_name_http_router = "http_router"
+  location                  = "europe-west2"
 }
 
-resource "google_storage_bucket" "this" {
-  name                        = data.google_project.this.number
-  location                    = local.location
-  uniform_bucket_level_access = true
-  force_destroy               = true
-}
-
-data "archive_file" "http_trigger" {
+data "archive_file" "http_router" {
   type        = "zip"
-  source_dir  = "${path.module}/../src/${local.function_name_http_trigger}"
-  output_path = "/tmp/${local.function_name_http_trigger}.zip"
+  source_dir  = "${path.module}/../src/build/${local.function_name_http_router}"
+  output_path = "/tmp/${local.function_name_http_router}.zip"
 }
 
-resource "google_storage_bucket_object" "http_trigger" {
-  name   = "${local.function_name_http_trigger}.zip"
-  bucket = google_storage_bucket.this.name
-  source = data.archive_file.http_trigger.output_path
+resource "google_storage_bucket_object" "http_router" {
+  name   = "${local.function_name_http_router}.zip"
+  bucket = google_storage_bucket.cloud_functions.name
+  source = data.archive_file.http_router.output_path
 }
 
-resource "google_cloudfunctions2_function" "http_trigger" {
-  name        = "http-trigger"
+data "google_secret_manager_secret" "http_router" {
+  secret_id = "SLACK_SIGNING_SECRET"
+}
+
+resource "google_secret_manager_secret_iam_binding" "http_router" {
+  secret_id = data.google_secret_manager_secret.http_router.secret_id
+  role      = "roles/secretmanager.secretAccessor"
+  members   = [
+    "serviceAccount:${google_service_account.http_router.email}",
+  ]
+}
+
+resource "google_cloudfunctions2_function" "http_router" {
+  name        = replace(local.function_name_http_router, "_", "-")
   location    = local.location
   description = "http trigger function"
 
   build_config {
     runtime     = "go119"
-    entry_point = "HelloWorld" # Set the entry point
+    entry_point = "Router"
     source {
       storage_source {
-        bucket = google_storage_bucket.this.name
-        object = google_storage_bucket_object.http_trigger.name
+        bucket = google_storage_bucket.cloud_functions.name
+        object = google_storage_bucket_object.http_router.name
       }
     }
   }
   service_config {
-    max_instance_count = 1
-    available_memory   = "256M"
-    timeout_seconds    = 60
+    max_instance_count    = 1
+    available_memory      = "256M"
+    timeout_seconds       = 60
+    environment_variables = {
+      PROJECT_ID = data.google_project.this.number
+      TOPIC_NAME = google_pubsub_topic.request.name
+    }
+    secret_environment_variables {
+      key        = "SLACK_SIGNING_SECRET"
+      project_id = data.google_project.this.number
+      secret     = data.google_secret_manager_secret.http_router.secret_id
+      version    = "latest"
+    }
+
+    service_account_email = google_service_account.http_router.email
   }
 
   lifecycle {
     replace_triggered_by = [
-      google_storage_bucket_object.http_trigger.md5hash
+      google_storage_bucket_object.http_router.md5hash
     ]
   }
 }
 
-resource "google_cloud_run_service_iam_binding" "http_trigger" {
-  location = google_cloudfunctions2_function.http_trigger.location
-  service  = google_cloudfunctions2_function.http_trigger.name
+resource "google_service_account" "http_router" {
+  account_id = replace(local.function_name_http_router, "_", "-")
+}
+
+resource "google_cloud_run_service_iam_member" "http_router" {
+  location = google_cloudfunctions2_function.http_router.location
+  service  = google_cloudfunctions2_function.http_router.name
   role     = "roles/run.invoker"
-  members  = [
-    "allUsers"
-  ]
+  member   = "allUsers"
+}
+
+resource "google_cloudfunctions2_function_iam_member" "http_router" {
+  location       = google_cloudfunctions2_function.http_router.location
+  cloud_function = google_cloudfunctions2_function.http_router.name
+  role           = "roles/cloudfunctions.invoker"
+  member         = "allUsers"
+}
+
+resource "google_pubsub_topic" "request" {
+  name = "acg-request"
+}
+
+resource "google_pubsub_topic_iam_member" "request" {
+  topic  = google_pubsub_topic.request.name
+  role   = "roles/pubsub.editor"
+  member = "serviceAccount:${google_service_account.http_router.email}"
 }
 
 output "function_uri" {
-  value = google_cloudfunctions2_function.http_trigger.service_config[0].uri
+  value = google_cloudfunctions2_function.http_router.service_config[0].uri
 }
