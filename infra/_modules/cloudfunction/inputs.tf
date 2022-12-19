344076e793ea1562549baffefd64acd58b65a508
Richard Weston <github@riweston.io>
2022-12-19 09:34:06 +0000

    feat: Add some IaC modules

---
+++ infra/_modules/cloudfunction/inputs.tf
@@ -0,0 +1,30 @@
+variable "source_dir" {}
+
+variable "function_name" {
+  default = ""
+}
+
+variable "bucket_name" {
+  default = ""
+}
+
+variable "location" {
+  default = ""
+}
+
+variable "project_id" {
+  default = ""
+}
+
+variable "environment_variables" {
+  default = null
+  type    = map(string)
+}
+
+variable "secret_environment_variables" {
+  default = []
+  type = list(object({
+    key          = string,
+    secret_value = string,
+  }))
+}
---
+++ infra/_modules/cloudfunction/outputs.tf
@@ -0,0 +1,7 @@
+output "function_name" {
+  value = google_cloudfunctions2_function.this.name
+}
+
+output "service_account_email" {
+  value = google_cloudfunctions2_function.this.service_config[0].service_account_email
+}
