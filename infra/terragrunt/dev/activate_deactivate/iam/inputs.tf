3e0d1305b95587ed4399e67b81c5035d113d5a76
Richard Weston <github@riweston.io>
2023-01-10 14:31:59 +0000

    feat: Stage the cloud infra

---
+++ infra/terragrunt/dev/activate_deactivate/iam/inputs.tf
@@ -0,0 +1,7 @@
+variable "function_name" {}
+variable "function_location" {}
+variable "function_service_account_email" {}
+variable "secret_id_acloudguru_api_key" {}
+variable "secret_id_acloudguru_consumer_id" {}
+variable "secret_id_slack_bot_token" {}
+variable "topic_name" {}
