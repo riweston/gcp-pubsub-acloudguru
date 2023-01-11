205873a56fb07f62e7a8147476e4780c4f3d9a04
Richard Weston <github@riweston.io>
2023-01-11 16:48:07 +0000

    fix: Update infra to provide the additional secrets required by lookup_slack_id

--- infra/terragrunt/dev/lookup_slack_id/iam/secrets.tf
+++ infra/terragrunt/dev/lookup_slack_id/iam/secrets.tf
@@ -1,5 +1,11 @@
 resource "google_secret_manager_secret_iam_member" "this" {
-  secret_id = var.secret_id
+  for_each = toset([
+    var.secret_id_slack_bot_token,
+    var.secret_id_acloudguru_api_key,
+    var.secret_id_acloudguru_consumer_id,
+  ])
+
+  secret_id = each.value
   role      = "roles/secretmanager.secretAccessor"
   member    = "serviceAccount:${var.function_service_account_email}"
 }
